Fix case with data covariance

combinetf2/fitter.py
@@ -539,6 +539,41 @@ class Fitter:
 
         return impacts, impacts_grouped
 
+    def _pd2ldbeta2(self, profile=False):
+        with tf.GradientTape(watch_accessed_variables=False) as t2:
+            t2.watch([self.ubeta])
+            with tf.GradientTape(watch_accessed_variables=False) as t1:
+                t1.watch([self.ubeta])
+                if profile:
+                    val = self._compute_loss(profile=profile)
+                else:
+                    # TODO this principle can probably be generalized to other parts of the code
+                    # to further reduce special cases
+
+                    # if not profiling, likelihood doesn't include the data contribution
+                    lc = self._compute_lc()
+                    if self.binByBinStat:
+                        _1, _2, beta = self._compute_yields_with_beta(
+                            profile=True, compute_norm=False, full=False
+                        )
+                        lbeta, _ = self._compute_lbeta(beta, profile=True)
+                        val = lc + lbeta
+                    else:
+                        val = lc
+            pdldbeta = t1.gradient(val, self.ubeta)
+        if self.externalCovariance:
+            pd2ldbeta2_matrix = t2.jacobian(pdldbeta, self.ubeta)
+            pd2ldbeta2 = tf.linalg.LinearOperatorFullMatrix(
+                pd2ldbeta2_matrix, is_self_adjoint=True
+            )
+        else:
+            # pd2ldbeta2 is diagonal, so we can use gradient instead of jacobian
+            pd2ldbeta2_diag = t2.gradient(pdldbeta, self.ubeta)
+            pd2ldbeta2 = tf.linalg.LinearOperatorDiag(
+                pd2ldbeta2_diag, is_self_adjoint=True
+            )
+        return pd2ldbeta2
+
     def _chi2(
         self,
         fun,
@@ -579,42 +614,8 @@ class Fitter:
         res_cov = pdresdx @ cov_dresdx
 
         if pdresdbeta is not None:
-            with tf.GradientTape(watch_accessed_variables=False) as t2:
-                t2.watch(self.ubeta)
-                with tf.GradientTape(watch_accessed_variables=False) as t1:
-                    t1.watch(self.ubeta)
-                    if profile:
-                        val = self._compute_loss(profile=profile)
-                    else:
-                        # TODO this principle can probably be generalized to other parts of the code
-                        # to further reduce special cases
-
-                        # if not profiling, likelihood doesn't include the data contribution
-                        lc = self._compute_lc()
-                        if self.binByBinStat:
-                            _1, _2, beta = self._compute_yields_with_beta(
-                                profile=True, compute_norm=False, full=False
-                            )
-                            lbeta, _ = self._compute_lbeta(beta, profile=True)
-                            val = lc + lbeta
-                        else:
-                            val = lc
-
-                pdldbeta = t1.gradient(val, self.ubeta)
-            if self.externalCovariance:
-                pd2ldbeta2_matrix = t2.jacobian(pdldbeta, self.ubeta)
-                pd2ldbeta2 = tf.linalg.LinearOperatorFullMatrix(
-                    pd2ldbeta2_matrix, is_self_adjoint=True
-                )
-            else:
-                # pd2ldbeta2 is diagonal, so we can use gradient instead of jacobian
-                pd2ldbeta2_diag = t2.gradient(pdldbeta, self.ubeta)
-                pd2ldbeta2 = tf.linalg.LinearOperatorDiag(
-                    pd2ldbeta2_diag, is_self_adjoint=True
-                )
-
+            pd2ldbeta2 = self._pd2ldbeta2(profile)
             pd2ldbeta2_pdresdbeta = pd2ldbeta2.solve(pdresdbeta, adjoint_arg=True)
-
             res_cov += pdresdbeta @ pd2ldbeta2_pdresdbeta
 
         # data stat part
@@ -633,9 +634,7 @@ class Fitter:
 
             if self.externalCovariance:
                 data_cov = tf.math.sqrt(tf.linalg.inv(self.data_cov_inv))
-                res_cov += dresdnobs @ tf.matmul(
-                    data_cov, tf.transpose(dresdnobs), transpose_b=True
-                )
+                res_cov += dresdnobs @ tf.matmul(data_cov, dresdnobs, transpose_b=True)
             else:
                 dnobs = tf.math.sqrt(self.nobs)
                 dresdnobs *= dnobs[None, :]
@@ -762,39 +761,7 @@ class Fitter:
             expcov = None
 
         if pdexpdbeta is not None:
-            with tf.GradientTape(watch_accessed_variables=False) as t2:
-                t2.watch([self.ubeta])
-                with tf.GradientTape(watch_accessed_variables=False) as t1:
-                    t1.watch([self.ubeta])
-                    if profile:
-                        val = self._compute_loss(profile=profile)
-                    else:
-                        # TODO this principle can probably be generalized to other parts of the code
-                        # to further reduce special cases
-
-                        # if not profiling, likelihood doesn't include the data contribution
-                        lc = self._compute_lc()
-                        if self.binByBinStat:
-                            _1, _2, beta = self._compute_yields_with_beta(
-                                profile=True, compute_norm=False, full=False
-                            )
-                            lbeta, _ = self._compute_lbeta(beta, profile=True)
-                            val = lc + lbeta
-                        else:
-                            val = lc
-                pdldbeta = t1.gradient(val, self.ubeta)
-            if self.externalCovariance:
-                pd2ldbeta2_matrix = t2.jacobian(pdldbeta, self.ubeta)
-                pd2ldbeta2 = tf.linalg.LinearOperatorFullMatrix(
-                    pd2ldbeta2_matrix, is_self_adjoint=True
-                )
-            else:
-                # pd2ldbeta2 is diagonal, so we can use gradient instead of jacobian
-                pd2ldbeta2_diag = t2.gradient(pdldbeta, self.ubeta)
-                pd2ldbeta2 = tf.linalg.LinearOperatorDiag(
-                    pd2ldbeta2_diag, is_self_adjoint=True
-                )
-
+            pd2ldbeta2 = self._pd2ldbeta2(profile)
             pd2ldbeta2_pdexpdbeta = pd2ldbeta2.solve(pdexpdbeta, adjoint_arg=True)
 
             if compute_cov: